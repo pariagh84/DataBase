ec87815a6497824a94c68b998ed8ecd619076180
Paria Gharakhani <pariagharakhani@gmail.com>
2025-03-21 23:00:56 +0330

    create Database class

---
+++ src/db/Database.java
@@ -0,0 +1,40 @@
+package db;
+
+import java.util.ArrayList;
+
+import static java.lang.reflect.Array.set;
+
+public class Database {
+
+    private static ArrayList<Entity> entities = new ArrayList<>();
+
+    public static void add(Entity e) {
+        entities.add(e);
+        e.id = entities.indexOf(e) + 1;
+    }
+
+    public static Entity get(int id) throws EntityNotFoundException {
+        for (Entity e : entities) {
+            if (e.id == id) {
+                return e;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void delete(int id) throws EntityNotFoundException {
+        entities.removeIf(e -> e.id == id);
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void update(Entity e) throws EntityNotFoundException {
+        for (Entity entity : entities) {
+            if (entity.id == e.id) {
+                int index = entities.indexOf(entity);
+                entities.set(index, e);
+                return;
+            }
+        }
+        throw new EntityNotFoundException(e.id);
+    }
+}
